299bbfe19c7df2d4c75c6319a45cf0d3f524e111
Lord-Lelsers <millan.kumar.24@bishops.com>
2021-10-14 10:39:17 -0700

    image switcher for product pages

--- Conja-Madoodle/script.js
+++ Conja-Madoodle/script.js
@@ -1,11 +1,6 @@
-// function toGreen(id) {
 
-//     console.log("button clicked: " + id);
-
-//     but = document.getElementById(id);
-
-//     if (but.classList.contains("btn-info")){
-//         but.classList.remove("btn-info");
-//         but.classList.add("btn-success");
-//     }
-// }
+function setPic(source, id) {
+    console.log("clicked")
+    img = document.getElementById(id);
+    img.src = source;
+}
