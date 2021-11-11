9fa0ac12fb6a1f3ee375992622f6cb496fe0240a
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2021-11-11 07:00:41 -0800

    trying to fix water (no luck yet)

--- 80sGame/TODO.md
+++ 80sGame/TODO.md
@@ -5,6 +5,7 @@
     - random numbered sections
 - Water spawns in "packs"
     - Fix spawn from top
+    - Keep the same "rows" as the cars?
 - If player goes back, things from top do not go to the bottom
 - General math/speed/sizes/etc
 - "Clean", Mr. Bloom approved code
--- 80sGame/newScript.js
+++ 80sGame/newScript.js
@@ -141,12 +141,16 @@ playerLevel = carHeight * 9;
 player = new Player(new Vector(canvas.width/2, playerLevel + 1.25 * carHeight), "#00ff00", carHeight, carHeight, carHeight * 1.5);
 
 cars = [];
+// waterBlockCount = 5;
 base = playerLevel - 1.75 * carHeight;
 for (var i = 0; i < 10; i++) {
     startPos = new Vector(getRandomInt(0, canvas.width - carWidth), base - (1.5 * carHeight * i));
     speed = (getRandomInt(i/4 + 1, i/3 + 2)/900) * canvas.width;
     speed = getRandomInt(1, 3) == 2? -speed : speed;
     cars.push(new Car(startPos, "#ff0000", carWidth, carHeight, speed));
+    // if (Math.random() > waterBlockCount/10) {
+    //     cars.push(new Water(startPos, "#0000ff", carWidth, carHeight));
+    // }
 }
 
 // to make it look like player is moving
