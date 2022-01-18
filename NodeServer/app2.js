fedb3ee0ccad278d74741a9398270547dab9db9d
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-01-18 11:59:18 -0800

    trying node js webserver

---
+++ NodeServer/app2.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileuploaded') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = './' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<p>waiting for JSON</p>");
+    return res.end();
+  }
+}).listen(8080);
