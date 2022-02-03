10735235152cc0a3aafbc4bcfe54d0e8b1f6b27d
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-02-03 09:28:42 -0800

    working on views

--- Django/NeoCrosserLauncher/NeoCrosser/urls.py
+++ Django/NeoCrosserLauncher/NeoCrosser/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     # ex: /neocrosser/signup/
     path('signup/', views.signup, name='signup'),
     # ex: /neocrosser/login/
-    path('login', views.login, name='login'),
+    path('login/', views.login, name='login'),
     # ex: /neocrosser/scores/
-    path('scores', views.scores, name='scores'),
+    path('scores/', views.scores, name='scores'),
     # ex: /neocrosser/game/
     path('game/', views.game, name='game'), # the actual NEO CROSSER PAGE
 ]
--- Django/NeoCrosserLauncher/NeoCrosser/views.py
+++ Django/NeoCrosserLauncher/NeoCrosser/views.py
@@ -15,12 +15,16 @@ def login(request):
     return HttpResponse("LOGIN")
 
 def scores(request):
-    response = "<ul>"
-    s = TopScore.objects.all()
-    for score in s:
-        response += "<li>RANK %i) %i</li>" % (score.getRank(), score.score)
-    response += "</ul>"
-    return HttpResponse(response)
+    # response = "<ul>"
+    # s = TopScore.objects.all()
+    # for score in s:
+    #     response += "<li>RANK %i) %i</li>" % (score.getRank(), score.score)
+    # response += "</ul>"
+    # return HttpResponse(response)
+
+    s = TopScore.objects.order_by('-score')
+    context = {'scores_list': s}
+    return render(request, 'NeoCrosser/scores.html', context)
 
 def game(request):
     return HttpResponse("Play NEOCROSSER")
