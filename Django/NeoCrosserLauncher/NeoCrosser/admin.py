139a56a9d8ee9fb8ce3f88f12c97ee4d21621757
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-02-16 12:00:05 -0800

    adding GameSave to admin

--- Django/NeoCrosserLauncher/NeoCrosser/admin.py
+++ Django/NeoCrosserLauncher/NeoCrosser/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Account, TopScore
+from .models import Account, TopScore, GameSave
 
 admin.site.register(Account)
 admin.site.register(TopScore)
+admin.site.register(GameSave)
