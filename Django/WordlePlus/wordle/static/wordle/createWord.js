b81e044c48e1c97f92c4ad5251f849fe0c112b58
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-02-24 09:04:38 -0800

    fixing the allowing 3-10 as valid options

--- Django/WordlePlus/wordle/static/wordle/createWord.js
+++ Django/WordlePlus/wordle/static/wordle/createWord.js
@@ -4,12 +4,11 @@ function toggle() {doubleLetters = !doubleLetters;}
 
 function SPCreateWord() {
     wordLen = document.getElementById("wordLen").value;
-    if (wordLen > 2 && wordLen < 10) {
+    if (wordLen >= 3 && wordLen <= 10) {
         document.getElementById("wordLenSub").value = wordLen;
         document.getElementById("triesSub").value = document.getElementById("tries").value;
         document.getElementById("doubleLettersSub").value = doubleLetters;
         document.getElementById("wordGen").submit();
-        console.log("Asdasd");
     }
     else document.getElementById("wordLen").style.border = "1px solid red";
 }
