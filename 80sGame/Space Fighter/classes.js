f15877fb9a6818491ee2fc5d8bbf638da7f6f275
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-03-05 12:33:11 -0800

    working on space fighter again

--- 80sGame/Space Fighter/classes.js
+++ 80sGame/Space Fighter/classes.js
@@ -78,16 +78,15 @@ class HitBox {
     }
 }
 
-class Spaceship {
+class Ship {
     constructor(pt, len) {
         this.pt = pt;
         this.angle = 0;
         this.len = len;
 
         this.turnSpeed = 3;
-        this.thrustSpeed = 0.01;
-        this.maxThrust = 0.02;
-        this.maxSpeed = 4.0;
+        this.thrust = 0.002;
+        this.vel = 0.0;
  
         this.moveVector = new Vector(0, 0);
 
@@ -96,116 +95,30 @@ class Spaceship {
     }
     move() {
         if (wDown) {
-            this.thrustSpeed += 0.001;
+            this.vel += this.thrust;
         }
-        if (sDown) {
-            this.thrustSpeed -= 0.001;
+        else {
+            this.vel -= this.thrust / 0.5;
         }
         if (dDown) {
-            this.angle -= this.turnSpeed;
-        }
-        if (aDown) {
             this.angle += this.turnSpeed;
         }
-
-        if (this.thrustSpeed < 0) this.thrustSpeed = 0; // no reverse
-        else if (this.thrustSpeed > this.maxThrust) this.thrustSpeed = this.maxThrust;
-
-        this.moveVector.apply(new Vector(-Math.sin(degToRad(this.angle)) * this.thrustSpeed, -Math.cos(degToRad(this.angle)) * this.thrustSpeed));
-        let currentSpeed = this.moveVector.calcLen();
-        if (currentSpeed > this.maxSpeed) this.moveVector.scale(this.maxSpeed);
-        // this.pt.apply(this.moveVector);
-        
-        for (var i = 0; i < asteroids.length; i++) {
-            asteroids[i].pt.apply(this.moveVector);
+        if (aDown) {
+            this.angle -= this.turnSpeed;
         }
+        this.moveVector.apply(new Vector(Math.cos(degToRad(this.angle)) * this.vel, Math.sin(degToRad(this.angle)) * this.vel));
+        this.pt.apply(this.moveVector);
     }
     draw() {
         context.fillStyle = this.color;
+        context.strokeStyle = this.color;
         context.beginPath();
-        
         context.moveTo(this.pt.x, this.pt.y);
-        context.lineTo(this.pt.x - Math.sin(degToRad(this.angle)) * this.len, this.pt.y - Math.cos(degToRad(this.angle)) * this.len);
-        context.lineTo(this.pt.x - Math.sin(degToRad(this.angle)) * this.len + Math.sin(degToRad(90 + this.angle)) * this.len/2, this.pt.y - Math.cos(degToRad(this.angle)) * this.len + Math.cos(degToRad(90 + this.angle)) * this.len/2);
-        
-        context.lineTo(this.pt.x, this.pt.y);
-        context.lineTo(this.pt.x - Math.sin(degToRad(this.angle)) * this.len, this.pt.y - Math.cos(degToRad(this.angle)) * this.len);
-        context.lineTo(this.pt.x - Math.sin(degToRad(this.angle)) * this.len - Math.sin(degToRad(90 + this.angle)) * this.len/2, this.pt.y - Math.cos(degToRad(this.angle)) * this.len - Math.cos(degToRad(90 + this.angle)) * this.len/2);
-        context.lineTo(this.pt.x, this.pt.y);
-        
-        context.fill();
-
-        // this.hb.draw("#00ff00");
-        this.drawThrustBar();
-    }
-    drawThrustBar() {
-        let x = 0;
-        let w = 50;
-        let maxHeight = 200;
-        let y = canvas.height - maxHeight;
-
-        context.fillStyle = "grey";
-        context.beginPath();
-        context.rect(x, y, w, maxHeight);
-        context.fill();
-
-        context.fillStyle = "#00ff00";
-        context.beginPath();
-        let percent = this.thrustSpeed/this.maxThrust;
-        context.rect(x, canvas.height - maxHeight * percent, w, maxHeight * percent);
-        context.fill();
+        context.lineTo(this.pt.x + Math.cos(degToRad(this.angle)) * (this.vel + 0.5) * 200, this.pt.y + Math.sin(degToRad(this.angle)) * (this.vel + 0.5) * 200);
+        context.stroke();
     }
 
     // checkHit(hb) {
         
     // }
-}
-
-
-class Asteroid {
-    constructor() {
-        this.w = 50;
-        this.h = 50;
-
-        this.canSpawn = true;
-        this.hasSpawned = false;
-
-        let badPt = true;
-        this.pt = new Vector(-1, -1);
-        while (badPt) {
-            badPt = false;
-            this.pt = new Vector(getRandomInt(0, canvas.width - this.w), getRandomInt(0, canvas.height - this.h));
-            for (var i = 0; i < asteroids.length; i++) {
-                if (new HitBox(this.pt, this.w, this.h).checkCollide(asteroids[i].hb)) {
-                    badPt = true;
-                }
-                // if (new HitBox(tempPt, this.w, this.h).checkCollide(PLAYER.HB))
-            }
-        }
-        this.color = "#ff0000";
-        this.hb = new HitBox(this.pt, this.w, this.h);
-
-        console.log("Asteroid created... (" + this.pt.x + "," + this.pt.y + ")");
-    }
-    update() {
-        if (this.hb.outOfBounds()) {
-            if (this.canSpawn && !this.hasSpawned) {
-                let tempAsteroid = new Asteroid();
-
-
-                tempAsteroid.canSpawn = false;
-                this.hasSpawned = true;
-                asteroids.push(tempAsteroid);
-            }
-        }
-        else {
-            this.canSpawn = true;
-        }
-    }
-    draw() {
-        context.fillStyle = this.color;
-        context.beginPath();
-        context.rect(this.pt.x, this.pt.y, this.w, this.h);
-        context.fill();
-    }
 }
--- 80sGame/Space Fighter/game.js
+++ 80sGame/Space Fighter/game.js
@@ -3,48 +3,45 @@ var sDown = false;
 var aDown = false;
 var dDown = false;
 
-
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
-    switch (e.key) {
-        case "w": case "ArrowUp":
+    switch (e.key.toLowerCase()) {
+        case "w": case "arrowup":
             wDown = true;
             break;
-        case "s": case "ArrowDown":
+        case "s": case "arrowdown":
             sDown = true;
             break;
-        case "a": case "ArrowLeft":
+        case "a": case "arrowleft":
             aDown = true;
             break;
-        case "d": case "ArrowRight":
+        case "d": case "arrowright":
             dDown = true;
             break;
         case "z":
             reset();
             break;
     }
-    inputMode = "key";
 }
 function keyUpHandler(e) {
     switch (e.key) {
-        case "w": case "ArrowUp": wDown = false; break;
-        case "s": case "ArrowDown": sDown = false; break;
-        case "a": case "ArrowLeft": aDown = false; break;
-        case "d": case "ArrowRight": dDown = false; break;
+        case "w": case "arrowup": wDown = false; break;
+        case "s": case "arrowdown": sDown = false; break;
+        case "a": case "arrowleft": aDown = false; break;
+        case "d": case "arrowright": dDown = false; break;
     }
 }
 
 function reset() {
-    console.log("restarting...");
     location.reload(); // reloads the webpage
 }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    value = Math.floor(Math.random() * (max - min) + min); //The max is exclusive and the min is inclusive
+    let value = Math.floor(Math.random() * (max - min) + min); //The max is exclusive and the min is inclusive
     return value;
 }
 
@@ -55,18 +52,16 @@ function degToRad(deg) {
     return deg * Math.PI / 180;
 }
 
+function airResistance(speed) {
+    return speed * 0.8;
+}
+
 function drawAll() {
     context.fillStyle = "#000000";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     player.move();
     player.draw();
-
-    for (var i = 0; i < asteroids.length; i++) {
-        asteroids[i].update();
-        asteroids[i].draw();
-    }
-    // console.log("current time: " + new Date().getTime() / 1000);
 
     window.requestAnimationFrame(drawAll);
 }
@@ -84,24 +79,11 @@ function setUpContext() {
     return context;
 }
 
-var startTime = new Date().getTime() / 1000;
-console.log("starting... (" + startTime + ")");
-
+var canvas;
 var context = setUpContext();
 
-var player = new Spaceship(new Vector(canvas.width/2, canvas.height/2), 30);
+var player = new Ship(new Vector(canvas.width/2, canvas.height/2), 30);
 
-var asteroids = [];
-
-for (var i = 0; i < 10; i++) {
-    let tempAsteroid = new Asteroid();
-    asteroids.push(tempAsteroid);
-}
-
-var endTime = new Date().getTime() / 1000;
-console.log("start up finished... (" + endTime + ")");
-
-console.log("loading time: " + (endTime - startTime));
 
 // Fire up the animation engine
 window.requestAnimationFrame(drawAll);
