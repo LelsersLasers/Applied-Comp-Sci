86819615808fe29580b160ef44458e92f15dc191
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-04-18 09:47:07 -0700

    snake_case + name -> cup

--- Django/WordlePlus/wordle/models.py
+++ Django/WordlePlus/wordle/models.py
@@ -14,17 +14,17 @@ class Account(models.Model):
 class Word(models.Model):
     txt = models.CharField("Word", max_length=200)
     length = models.IntegerField("Length")
-    doubleLetters = models.BooleanField("DoubleLetters", default=False)
+    double_letters = models.BooleanField("Does the word have double letters?", default=False)
 
     def __str__(self):
         return "%i: %s (%s)" % (self.length, self.txt, self.doubleLetters)
 
 class Score(models.Model):
-    name = models.CharField("Compition Name", max_length=200)
+    cup = models.CharField("Cup Name", max_length=200)
     word = models.ManyToManyField(Word)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    guesses = models.IntegerField("Guesses")
-    time = models.IntegerField("Time in seconds")
+    guesses = models.IntegerField("Guesses taken to solve")
+    time = models.IntegerField("Time in seconds taken to solve")
     sub_date = models.DateTimeField("Date Submitted")
 
     def __str__(self):
@@ -40,9 +40,8 @@ class Score(models.Model):
         wordsTxt = wordsTxt[:-1]
         return "%s) %s - %s) '%s' in %i guesses and %02i:%02i" % (self.name, self.sub_date, self.account.display_name, wordsTxt, self.guesses, minutes, sec)
 
-
     def checkInTimeFrame(self):
-        if "daily" in self.name.lower():
-            tMidNight = (int(timezone.now().timestamp() // 86400)) * 86400
-            return tMidNight <= int(self.sub_date.timestamp())
+        if "daily" in self.cup.lower():
+            last_midnight = (int(timezone.now().timestamp() // 86400)) * 86400
+            return last_midnight <= int(self.sub_date.timestamp())
         return True
