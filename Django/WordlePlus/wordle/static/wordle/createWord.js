8609aa46a5722274636cdf2015ef49b6037e2c39
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-05-03 11:06:26 -0700

    improving JS / SP launcher failure because of out of bounds inputs

--- Django/WordlePlus/wordle/static/wordle/createWord.js
+++ Django/WordlePlus/wordle/static/wordle/createWord.js
@@ -8,14 +8,18 @@ function toggle(id) {
 function SPCreateWord() {
     wordLen = document.getElementById("wordLen").value;
     tries = document.getElementById("tries").value
-    if (wordLen >= 3 && wordLen <= 10) {
-        if (tries <= 100) {
-            document.getElementById("wordLenSub").value = wordLen;
-            document.getElementById("triesSub").value = tries;
-            document.getElementById("doubleLettersSub").value = checkboxes[0];
-            document.getElementById("commonSub").value = checkboxes[1];
-            document.getElementById("wordGen").submit();
-        }
+    
+    if (wordLen < 3 || wordLen > 10) document.getElementById("wordLen").style.border = "1px solid red";
+    else document.getElementById("wordLen").style.border = "none";
+    
+    if (tries > 100) document.getElementById("tries").style.border = "1px solid red";
+    else document.getElementById("tries").style.border = "none";
+
+    if (wordLen >= 3 && wordLen <= 10 && tries <= 100) {
+        document.getElementById("wordLenSub").value = wordLen;
+        document.getElementById("triesSub").value = tries;
+        document.getElementById("doubleLettersSub").value = checkboxes[0];
+        document.getElementById("commonSub").value = checkboxes[1];
+        document.getElementById("wordGen").submit();
     }
-    else document.getElementById("wordLen").style.border = "1px solid red";
 }
