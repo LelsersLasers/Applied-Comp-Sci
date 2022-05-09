44c044b79f2513842a0f5e68549ac2978a5cf9e6
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-05-09 08:38:38 -0700

    Putting JS and CSS inside the html
    reordered the template inheritance

--- Django/WordlePlus/WordlePlus/urls.py
+++ Django/WordlePlus/WordlePlus/urls.py
@@ -1,7 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('wordle.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
