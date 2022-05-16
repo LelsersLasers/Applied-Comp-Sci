14aeec1188af397106e42047014dc6d5582591d8
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-05-16 10:21:05 -0700

    Can now load secret key from env file (TODO: change secret key on server)

--- Django/WordlePlus/TODO.md
+++ Django/WordlePlus/TODO.md
@@ -3,6 +3,8 @@
 Prepare for full release
 - Firewall server away from wifi
 - Secret key
+    - Change to special key on server
+        - python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
     - https://dev.to/joshwizzy/how-to-keep-secrets-out-of-django-settings-41c4
     - https://django-environ.readthedocs.io/en/latest/
 - Reset database
--- Django/WordlePlus/WordlePlus/settings.py
+++ Django/WordlePlus/WordlePlus/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'TODO: Change into an enviroement variable'
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
+print(env('SECRET_KEY'))
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
