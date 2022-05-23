35d015634704167ef5c650c091c17f0b65d1d13b
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-05-23 16:13:28 -0700

    Improvements + adding input for double letters

--- Django/WordlePlus/TODO.md
+++ Django/WordlePlus/TODO.md
@@ -1,5 +1,9 @@
 # Todo for Wordle<sup>+</sup>
 
+Cheats.py:
+- Correctly do double letters
+- Add to README.md
+
 Prepare for full release
 - Firewall server away from wifi
 - reset DB/scores
--- Django/WordlePlus/wordle/cheats.py
+++ Django/WordlePlus/wordle/cheats.py
@@ -5,16 +5,18 @@ alphabet = list("abcdefghijklmnopqrstuvwxyz")
 
 
 def get_words():
-    while True:
-        try:
-            user_input = input("Only common words [y/n]? ")
-            choice = user_input[0].lower()
-            if choice == "y":
-                return allWords.get_common_words()
-            return allWords.get_all_words()
-        except:
-            print("Hint: enter 'y' or 'n'")
+    try:
+        user_input = input("Only common words [Y/n]? ")
+        if user_input[0].lower() == "n": return allWords.get_all_words()
+    except: pass
+    return allWords.get_common_words()
 
+def input_double_letters():
+    try:
+        user_input = input("Could there be double letters [Y/n]? ")
+        if user_input[0].lower() == "n": return False
+    except: pass
+    return True
 
 def input_word_len():
     print("How many letters are in the word?")
@@ -34,9 +36,9 @@ def input_green_letters():
     while True:
         try:
             user_input = input("Letter (leave blank to continue): ")
-            if len(user_input) == 0:
-                return letters
+            if len(user_input) == 0: return letters
             letter = user_input.lower()[0]
+            assert(letter in alphabet)
             user_input = input("Position: ")
             idx = int(user_input)
             assert(idx > 0)
@@ -51,17 +53,23 @@ def input_yellow_letters():
     while True:
         try:
             user_input = input("Letter (leave blank to continue): ")
-            if len(user_input) == 0:
-                return letters
+            if len(user_input) == 0: return letters
             letter = user_input.lower()[0]
+            assert(letter in alphabet)
             letters.append(letter)
         except:
-            print("Hint: you are doing something wrong (maybe read the directions?)")
+            print("Hint: you are doing something wrong (enter only letters)")
 
 def only_neg(old, new):
-    if not old:
-        return old
+    if not old: return old
     return new
+
+def has_double_letters(word):
+    letters = []
+    for letter in word:
+        if letter in letters: return True
+        else: letters.append(letter)
+    return False
 
 def bold_text(txt):
     print("#" * (len(txt) + 4))
@@ -78,6 +86,8 @@ def main():
     print("\n") 
     words = get_words()
     print("\n") 
+    double_letters = input_double_letters()
+    print("\n") 
     word_len = input_word_len()
     print("\n")
     green_letters = input_green_letters()
@@ -87,24 +97,22 @@ def main():
     print("\nSEARCHING...")
     possible_words = []
     for word in words:
-        good = len(word) == word_len
+        good = len(word) == word_len and not (not double_letters and has_double_letters(word))
+        # letter_lst = list(word)
         for gl in green_letters:
-            try:
-                good = only_neg(good, word[gl[1]] == gl[0])
-            except:
-                continue
+            try: good = only_neg(good, word[gl[1]] == gl[0])
+            except: continue
         for yl in yellow_letters:
-            try:
-                temp = word.index(yl)
-            except:
-                good = False
-        if good:
-            possible_words.append(word)
+            good = only_neg(good, yl in word)
+        if good: possible_words.append(word)
     print("FINISHED SEARCHING...\n")
 
     bold_text("Possible Words:")
-    for i in range(len(possible_words)):
-        print("%i) %s" % (i + 1, possible_words[i]))
+    if len(possible_words) > 0:
+        for i in range(len(possible_words)):
+            print("%i) %s" % (i + 1, possible_words[i]))
+    else:
+        print("No words found. Did you mis-type or incorrectly enter information?")
 
     print("\nPROGRAM EXITING...\n")
 
