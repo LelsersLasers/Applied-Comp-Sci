99d0b3c993f1e25d521696026e395758aa76ee09
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-05-24 14:33:05 -0700

    Formatting helper

--- Django/WordlePlus/wordle/helper.py
+++ Django/WordlePlus/wordle/helper.py
@@ -6,15 +6,20 @@ alphabet = list("abcdefghijklmnopqrstuvwxyz")
 
 def get_words():
     only_common = input_yes_or_no("Only common words [Y/n]? ")
-    if only_common: return allWords.get_all_words()
+    if only_common:
+        return allWords.get_all_words()
     return allWords.get_common_words()
+
 
 def input_yes_or_no(prompt):
     try:
         user_input = input(prompt)
-        if user_input[0].lower() == "n": return False
-    except: pass
+        if user_input[0].lower() == "n":
+            return False
+    except:
+        pass
     return True
+
 
 def input_word_len():
     print("How many letters are in the word?")
@@ -22,28 +27,33 @@ def input_word_len():
         try:
             user_input = input("Length: ")
             num = int(user_input)
-            assert(num > 0)
+            assert num > 0
             return num
         except:
             print("Hint: enter a positive number!")
 
+
 def input_green_letters():
     print("Enter letters that you know the position of (aka the 'green' letters).")
-    print("First enter the letter, hit [enter] then enter the position of the letter where 1 is the first letter.")
+    print(
+        "First enter the letter, hit [enter] then enter the position of the letter where 1 is the first letter."
+    )
     letters = []
     while True:
         try:
             user_input = input("Letter (leave blank to continue): ")
-            if len(user_input) == 0: return letters
+            if len(user_input) == 0:
+                return letters
             letter = user_input.lower()[0]
-            assert(letter in alphabet)
+            assert letter in alphabet
             user_input = input("Position: ")
             idx = int(user_input)
-            assert(idx > 0)
+            assert idx > 0
             letters.append([letter, idx - 1])
             print("")
         except:
             print("Hint: you are doing something wrong (maybe read the directions?)")
+
 
 def input_letters(prompt):
     print(prompt)
@@ -51,51 +61,63 @@ def input_letters(prompt):
     while True:
         try:
             user_input = input("Letter (leave blank to continue): ")
-            if len(user_input) == 0: return letters
+            if len(user_input) == 0:
+                return letters
             letter = user_input.lower()[0]
-            assert(letter in alphabet)
+            assert letter in alphabet
             letters.append(letter)
         except:
             print("Hint: you are doing something wrong (enter only letters)")
 
-def only_neg(old, new):
-    if not old: return old
-    return new
 
 def has_double_letters(word):
     letters = []
     for letter in word:
-        if letter in letters: return True
-        else: letters.append(letter)
+        if letter in letters:
+            return True
+        else:
+            letters.append(letter)
     return False
+
 
 def bold_text(txt):
     print("#" * (len(txt) + 4))
     print("# " + txt + " #")
     print("#" * (len(txt) + 4))
 
+
 def run(word_len, words, double_letters):
     print("\nENTER INFORMATION...\n")
-    
+
     green_letters = input_green_letters()
     print("\n")
-    yellow_letters = input_letters("Enter letters that you are are in the word (aka the 'yellow' letters).")
+    yellow_letters = input_letters(
+        "Enter letters that you are are in the word (aka the 'yellow' letters)."
+    )
     print("\n")
     dark_letters = input_letters("Enter letters that are not in the word.")
 
     print("\nSEARCHING...")
     possible_words = []
     for word in words:
-        good = len(word) == word_len and not (not double_letters and has_double_letters(word))
-        for dl in dark_letters: good = good and dl not in word
+        good = len(word) == word_len and not (
+            not double_letters and has_double_letters(word)
+        )
+        for dl in dark_letters:
+            good = good and dl not in word
         letter_lst = list(word)
         for gl in green_letters:
             try:
-                if word[gl[1]] == gl[0]: letter_lst.remove(gl[0])
-                else: good = False
-            except: continue
-        for yl in yellow_letters: good = good and yl in letter_lst
-        if good: possible_words.append(word)
+                if word[gl[1]] == gl[0]:
+                    letter_lst.remove(gl[0])
+                else:
+                    good = False
+            except:
+                continue
+        for yl in yellow_letters:
+            good = good and yl in letter_lst
+        if good:
+            possible_words.append(word)
     print("FINISHED SEARCHING...\n")
 
     bold_text("Possible Words:")
@@ -109,20 +131,21 @@ def run(word_len, words, double_letters):
 def main():
     print("\nPROGRAM STARTING...\n")
     bold_text("Welcome to the wordle helper!")
-    
-    print("\n") 
+
+    print("\n")
     word_len = input_word_len()
     print("\n")
     words = get_words()
-    print("\n") 
+    print("\n")
     double_letters = input_yes_or_no("Could there be double letters [Y/n]? ")
 
     running = True
     while running:
         run(word_len, words, double_letters)
-        print("\n") 
+        print("\n")
         running = input_yes_or_no("Run again [Y/n]? ")
 
     print("\nPROGRAM EXITING...\n")
+
 
 main()
